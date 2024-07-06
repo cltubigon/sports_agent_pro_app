509132d733486de1f3a880b32e4a307649a20a71
cltubigon <cltubigon@gmail.com>
2024-07-07 02:21:40 +0800

    added dashboard and account loader2

--- src/app/(dashboard)/dashboard/loading.js
+++ src/app/(dashboard)/dashboard/loading.js
@@ -1,26 +1,29 @@
 'use client'
+import ContentContainerDashboard from '@/app/components/ThisWebsiteOnly/Dashboard/ContentContainerDashboard'
 import React from 'react'
 
 const DashboardLoading = () => {
   return (
-    <div className={'w-full'}>
-      <h2
-        className={
-          'text-xl font-semibold w-fit select-none text-transparent bg-neutral-200 animate-pulse'
-        }
-      >
-        Dashboard
-      </h2>
-      <p
-        className={
-          'w-fit mt-1 select-none text-transparent bg-neutral-200 animate-pulse'
-        }
-      >
-        In aliquid quia id provident optio et iure quae? Hic cumque internos qui
-        quis dolores ut dolor explicabo et voluptatem ullam qui dolorum enim qui
-        perspiciatis animi in dolores voluptas.
-      </p>
-    </div>
+    <ContentContainerDashboard>
+      <div className={'w-full'}>
+        <h2
+          className={
+            'text-xl font-semibold w-fit select-none text-transparent bg-neutral-200 animate-pulse'
+          }
+        >
+          Dashboard
+        </h2>
+        <p
+          className={
+            'w-fit mt-1 select-none text-transparent bg-neutral-200 animate-pulse'
+          }
+        >
+          In aliquid quia id provident optio et iure quae? Hic cumque internos
+          qui quis dolores ut dolor explicabo et voluptatem ullam qui dolorum
+          enim qui perspiciatis animi in dolores voluptas.
+        </p>
+      </div>
+    </ContentContainerDashboard>
   )
 }
 
