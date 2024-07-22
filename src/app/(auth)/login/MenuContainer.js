937bee37c1a25c9cb4e4c2d4cd494acab69ad1fb
cltubigon <cltubigon@gmail.com>
2024-07-22 20:02:16 +0800

    added preview of images3

--- src/app/(auth)/login/MenuContainer.js
+++ src/app/(auth)/login/MenuContainer.js
@@ -10,13 +10,13 @@ const MenuContainer = () => {
         'lg:flex flex-col bg-secondary w-full lg:max-w-[280px] xl:max-w-[390px] 2xl:max-w-[470px] xl:rounded-md px-8 py-4 lg:px-10 lg:py-10'
       }
     >
-      <div className={'w-fit mx-auto'}>
+      <div className={'w-fit max-sm:mx-auto'}>
         <Link href={'/'} prefetch className="w-fit">
           <Image
             src={saplogo}
             alt="Sports Agent Pro logo"
             quality={100}
-            className="lg:mb-5 mx-auto md:mx-[unset] max-w-[100px]"
+            className="lg:mb-5 mx-auto md:mx-[unset] max-w-[100px] md:max-w-[unset]"
           />
         </Link>
       </div>
